c14d971eec77bc922157d4b99c548080181b9c19
Umair Khan <ukn@comcast.net>
2020-05-31 18:30:59 -0700

    Change naming convention for real photos

--- real-photos/downsample.sh
+++ real-photos/downsample.sh
@@ -5,11 +5,11 @@
 
 for i in {0..7}
 do
-    convert original/$i.bmp -resize 840x840 z2/in_$i.bmp
-    convert original/$i.bmp -resize 560x560 z3/in_$i.bmp
-    convert original/$i.bmp -resize 420x420 z4/in_$i.bmp
-    convert original/$i.bmp -resize 336x336 z5/in_$i.bmp
-    convert original/$i.bmp -resize 280x280 z6/in_$i.bmp
-    convert original/$i.bmp -resize 240x240 z7/in_$i.bmp
-    convert original/$i.bmp -resize 210x210 z8/in_$i.bmp
+    convert original/$i.bmp -resize 840x840 z2/${i}_in.bmp
+    convert original/$i.bmp -resize 560x560 z3/${i}_in.bmp
+    convert original/$i.bmp -resize 420x420 z4/${i}_in.bmp
+    convert original/$i.bmp -resize 336x336 z5/${i}_in.bmp
+    convert original/$i.bmp -resize 280x280 z6/${i}_in.bmp
+    convert original/$i.bmp -resize 240x240 z7/${i}_in.bmp
+    convert original/$i.bmp -resize 210x210 z8/${i}_in.bmp
 done
